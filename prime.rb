941788db6e139ff1935c133aed55c52d95d5b68d
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 03:04:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(number)
   elsif number <= 0 || number == 1
     prime = false;
   else
-    int = [2, 3, 4, 5, 6, 7, 8, 9]
+    int = (2..number)
     int.each do |divisor|
        prime = false if number % divisor == 0 
     end 
