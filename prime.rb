7a7ae48fe1ac34db0200f8aa27f92dd4a31343ab
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 02:56:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   prime = true
   if number == 2 || number == 3 || number == 7
     prime = true 
-  elsif number <= 0 
+  elsif number <= 0 || number == 1
     prime = false;
   else
     int = [2, 3, 4, 5, 6, 7, 8, 9]
